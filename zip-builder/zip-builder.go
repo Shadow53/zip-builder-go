224a71aff5e893bf9e0de957b7bc0d13db404db7
Shadow53 <shadow53@shadow53.com>
2017-08-06 19:26:07 -0700

    Binary shouldn't default to being "main"

--- zip-builder/zip-builder.go
+++ zip-builder/zip-builder.go
@@ -4,12 +4,10 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
-	"path/filepath"
 
 	"github.com/spf13/viper"
 	"gitlab.com/Shadow53/zip-builder/build"
 	"gitlab.com/Shadow53/zip-builder/config"
-	"gitlab.com/Shadow53/zip-builder/lib"
 )
 
 func main() {
@@ -17,15 +15,9 @@ func main() {
 	dir, err := ioutil.TempDir("", "zip-builder-")
 	defer os.RemoveAll(dir)
 	viper.SetDefault("tempdir", dir)
-	// Get current working directory, set as default destination
-	ex, err := os.Executable()
-	lib.ExitIfError(err)
-	cwd := filepath.Dir(ex)
-	viper.SetDefault("destination", cwd)
 	// All config files must be called "build"...
 	viper.SetConfigName("build")
-	// ... and read from the current directory
-	viper.AddConfigPath(cwd)
+	viper.AddConfigPath(".")
 	err = viper.ReadInConfig()
 
 	if err != nil {
